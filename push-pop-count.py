d29c2d39df63362e8d65364e5abd5bb6370c26f9
Qua4rk <89824936+Qua4rk@users.noreply.github.com>
2021-09-03 22:46:27 -0400

    Update push-pop-count.py

--- push-pop-count.py
+++ push-pop-count.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify
-from werkzeug.wrappers import response #aquí importamos la librería Flask en nuestro archivo.
-app = Flask(__name__) #aquí creamos una nueva instancia del servidor Flask.
+from werkzeug.wrappers import response 
+app = Flask(__Ejercicio__) #aquí creamos una nueva instancia del servidor Flask.
 
-@app.route("/api/queue/push", methods=['POST']) # aquí especificamos que estos endpoints aceptan solicitudes POST y GET.
+@app.route("/api/queue/push", methods=['POST']) # aquí especificamos que este endpoint acepta solicitudes POST..
 def handle_api_push():
   if request.method == 'POST': # podemos entender qué tipo de request estamos manejando usando un condicional
     bodyrqst1 = {
@@ -10,7 +10,7 @@ def handle_api_push():
 }
     return jsonify(bodyrqst1)
     
-@app.route("/api/queue/pop", methods=['POST']) # aquí especificamos que estos endpoints aceptan solicitudes POST y GET.
+@app.route("/api/queue/pop", methods=['POST']) # aquí especificamos que este endpoint acepta solicitudes POST.
 def handle_api_pop():
   if request.method == 'POST': # podemos entender qué tipo de request estamos manejando usando un condicional
     bodyrqst2 = {
@@ -18,12 +18,12 @@ def handle_api_pop():
 }
     return jsonify(bodyrqst2)
 
-@app.route("/api/queue/count", methods=['GET']) # aquí especificamos que estos endpoints aceptan solicitudes POST y GET.
+@app.route("/api/queue/count", methods=['GET']) # aquí especificamos que este endpoint acepta solicitudes GET.
 def handle_api_count():
   if request.method == 'GET': # podemos entender qué tipo de request estamos manejando usando un condicional
     bodyrqst3 = {
 'status': 'ok',
 }
     return jsonify(bodyrqst3)
-
+    
 app.run(host='0.0.0.0') #finalmente iniciamos el servidor en el localhost.
